216108ca4d7f9616da4d5171fe36539ac48c2360
syphyr <syphyr@gmail.com>
2016-04-06 23:57:11 +0200

    Formatting fixes in go script

--- go.sh
+++ go.sh
@@ -1,13 +1,23 @@
 echo "Syncing Repos."
+echo ""
 ./repo_sync
+echo ""
 echo "Building ROM."
+echo ""
 ./start_build
+echo ""
 echo "Creating Changelog."
+echo ""
 ./make_changelog
+echo ""
 echo "Updating Web Server."
+echo ""
 ./update_web
+echo ""
 echo "OTA Unit Test."
+echo ""
 ota_unit_test
 echo ""
 echo "Done."
+echo ""
 

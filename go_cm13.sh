e20ca3478634c3f303a85e579a5d02838c9459ee
syphyr <syphyr@gmail.com>
2016-10-24 21:40:33 +0200

    Rename files to specify cm13 builds

--- go_cm13.sh
+++ go_cm13.sh
@@ -1,21 +1,21 @@
 sudo update-java-alternatives -s java-1.7.0-openjdk-amd64
 
-if [ ! -e ./repo_sync ]; then
+if [ ! -e ./repo_sync_cm13 ]; then
   echo "Repo sync script is not found"
   exit
 fi
 
-if [ ! -e ./start_build ]; then
+if [ ! -e ./start_build_cm13 ]; then
   echo "Start build script is not found"
   exit
 fi
 
-if [ ! -e ./make_changelog ]; then
+if [ ! -e ./make_changelog_cm13 ]; then
   echo "Make changelog script is not found"
   exit
 fi
 
-if [ ! -e ./update_web ]; then
+if [ ! -e ./update_web_cm13 ]; then
   echo "Update web script is not found"
   exit
 fi
@@ -23,17 +23,17 @@ fi
 echo ""
 echo "Syncing Repos."
 echo ""
-./repo_sync
+./repo_sync_cm13
 echo ""
 echo "Creating Changelog."
 echo ""
-./make_changelog
+./make_changelog_cm13
 echo ""
 echo "Building ROM."
 echo ""
-./start_build
+./start_build_cm13
 echo ""
 echo "Updating Web Server."
 echo ""
-./update_web
+./update_web_cm13
 

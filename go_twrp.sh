3446a988fef42ff9f011bc5a4a1c99e529e4f928
syphyr <syphyr@gmail.com>
2017-01-14 21:11:54 +0100

    Revert "Update TWRP build scripts to android-7.1 branch"
    Revert this commit until android-7.1 branch builds correctly
    for v500.
    
    This reverts commit 6f48e0459cc759327ab6e0229b44434e9cc246f5.

--- go_twrp.sh
+++ go_twrp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
-sudo update-alternatives --set javac /usr/lib/jvm/java-8-openjdk-amd64/bin/javac
+sudo update-alternatives --set java /usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java
+sudo update-alternatives --set javac /usr/lib/jvm/java-7-openjdk-amd64/bin/javac
 
 # Directory that contains CM build scripts
 BUILD_SCRIPT_DIR=~/android

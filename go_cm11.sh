ea05ed0991fb2505b11ec498e89b1b2803f85b88
syphyr <syphyr@gmail.com>
2017-05-31 01:42:13 +0200

    Reset back to java 8

--- go_cm11.sh
+++ go_cm11.sh
@@ -47,3 +47,5 @@ echo "Updating Web Server."
 echo ""
 ./update_web_cm11
 
+sudo update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
+sudo update-alternatives --set javac /usr/lib/jvm/java-8-openjdk-amd64/bin/javac

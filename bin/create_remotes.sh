80904e75020d0d9eecfd7bb1ace93f8f07179819
syphyr <syphyr@gmail.com>
2017-09-10 00:38:34 +0200

    Add remote creation script

---
+++ bin/create_remotes.sh
@@ -0,0 +1,11 @@
+cd ~/android/system/cm-14.1/kernel/samsung/msm8976
+
+git remote add aurora-msm-3.10 https://source.codeaurora.org/quic/la/kernel/msm-3.10
+git remote add aurora-msm-3.18 https://source.codeaurora.org/quic/la/kernel/msm-3.18
+git remote add aurora-msm-4.4 https://source.codeaurora.org/quic/la/kernel/msm-4.4
+git remote add aurora-qcacld https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan/qcacld-2.0
+git remote add cyanogen https://github.com/LineageOS/android_kernel_samsung_msm8976
+git remote add google-msm https://android.googlesource.com/kernel/msm.git
+git remote add stable git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
+
+
